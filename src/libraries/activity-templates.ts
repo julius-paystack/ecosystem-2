713fedf154fed0df0c7d3a1aa5a3ed9c40d80a34
Julius Ijie <julius@paystack.com>
2019-12-14 15:58:10 +0100

    Update activity templates

--- src/libraries/activity-templates.ts
+++ src/libraries/activity-templates.ts
@@ -1,6 +1,20 @@
 export const ActivityTemplates = [
 	{
 		id: 1,
-		name: 'Clean the '
-	}
+		name: 'Public Transport',
+		description: '',
+		points: 200,
+	},
+	{
+		id: 2,
+		name: 'No Cars',
+		desscription: '',
+		points: 500
+	},
+	{
+		id: 3,
+		name: 'Zero Plastic',
+		description: '',
+		points: 300,
+	},
 ];
